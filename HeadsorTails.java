518866dcf4050a9ddd856cfc5ba54c98291131c6
s1270035 <s1270035@u-aizu.ac.jp>
2020-08-06 10:17:56 +0900

    HoT: Initial revision

---
+++ HeadsorTails.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+
+public class HeadsorTails {
+
+  public static void main(String[] args) {
+    String[] CoinsSurface = { "Heads", "Tails" };
+    Random a = new Random();
+    
+    System.out.println("Tossing a coin...");
+    int countH = 0;
+    int countT = 0;
+    for(int i = 0 ; i < 5 ; i++ ) {
+      String CS = CoinsSurface[a.nextInt(2)];
+      System.out.print("Round " + (i + 1) + ": ");
+      System.out.println(CS);
+      if(CS == "Heads"){
+          countH++;
+      }
+      else countT++;
+    }
+    System.out.println("Heads: " + countH + ", Tails: " + countT);
+  }
+
+}
